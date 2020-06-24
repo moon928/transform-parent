7380accd133fdb1d4230c02129d5e9291efb4207
Yan <1243359973@qq.com>
2020-06-24 11:23:19 +0800

    随便写些些些

--- transform-boot/src/main/java/cn/trasen/mcpc/transform/service/impl/SpecsServiceImpl.java
+++ transform-boot/src/main/java/cn/trasen/mcpc/transform/service/impl/SpecsServiceImpl.java
@@ -1,12 +1,17 @@
 package cn.trasen.mcpc.transform.service.impl;
 
 import cn.trasen.mcpc.framework.base.BaseServiceImpl;
+import cn.trasen.mcpc.framework.util.IdWorker;
+import cn.trasen.mcpc.transform.dto.UscDrgSpecsDto;
 import cn.trasen.mcpc.transform.model.TTest;
 import cn.trasen.mcpc.transform.model.UscDictDetail;
 import cn.trasen.mcpc.transform.model.UscDrgSpecs;
 import cn.trasen.mcpc.transform.service.SpecsService;
 import cn.trasen.mcpc.transform.service.TTestService;
 import cn.trasen.mcpc.transform.service.UscDictDetailService;
+import cn.trasen.mcpc.transform.util.ReadExcelUtil;
+import cn.trasen.mcpc.transform.util.WriteFile;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +20,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,9 +38,17 @@ public class SpecsServiceImpl extends BaseServiceImpl<UscDrgSpecs> implements Sp
 
     private Map<String,String> detailMap = new HashMap<>();
 
+    /**
+     * 进口药品临时map
+     */
+    private Map<String,Long> jkypMap = new HashMap<String,Long>();
     @Autowired
     private UscDictDetailService uscDictDetailService;
 
+    //剂型 字典
+    String dosform = "DOSFORM";
+    //药品类型字典
+    String drugType = "drug_type";
     @Override
     public Mapper<UscDrgSpecs> getMapper() {
         return null;
@@ -43,7 +57,7 @@ public class SpecsServiceImpl extends BaseServiceImpl<UscDrgSpecs> implements Sp
     void initializeDetail(){
         List<UscDictDetail> list = uscDictDetailService.list(null);
         for (UscDictDetail uscDictDetail : list){
-//            detailMap.put(uscDictDetail.getCode()+uscDictDetail.getCatalogCode(),uscDictDetail.getName());
+
             detailMap.put(uscDictDetail.getName()+uscDictDetail.getCatalogCode(),uscDictDetail.getCode());
         }
     }
@@ -76,7 +90,76 @@ public class SpecsServiceImpl extends BaseServiceImpl<UscDrgSpecs> implements Sp
 
     @Override
     public String jkypToSql(String excelPath, String sqlPath) {
+        Map<String, List<UscDrgSpecsDto>> map = ReadExcelUtil.readXlsxForjkyp(excelPath);
+        List<UscDrgSpecsDto> success = map.get("success");
+        //有问题的不插入
+        List<UscDrgSpecsDto> notInsertDrug = new ArrayList<UscDrgSpecsDto>();
+        List<UscDrgSpecsDto> insertDrug = new ArrayList<UscDrgSpecsDto>();
+        Map<String, List<UscDrgSpecsDto>> drugData = new HashMap<String, List<UscDrgSpecsDto>>();
+        for (UscDrgSpecsDto item: success){
+            //标记 0-没有错误 1-有错误
+            int flag = 0;
+            String readError = "";
 
-        return null;
+            //是否进口
+            item.setImported(1);
+            //剂型 字典
+            String jxCode = isHave(item.getDrugDosformCode(), dosform);
+            if (StringUtils.isNotBlank(jxCode)){
+                item.setDrugDosformCode(jxCode);
+            }else {
+                readError += "--剂型" + item.getDrugDosformCode() + "字典中无匹配项";
+                flag = 1;
+            }
+            //药品类型 字典
+            String lxCode = isHave(item.getDrugType(), drugType);
+            if (StringUtils.isNotBlank(lxCode)){
+                item.setDrugType(lxCode);
+            }else {
+                readError += "--药品类型" + item.getDrugType() + "字典中无匹配项";
+                flag = 1;
+            }
+            if (flag==1){
+                item.setReadError(readError);
+                notInsertDrug.add(item);
+                continue;
+            }
+            if (jkypMap.get(item.getChemicalName()+item.getDrugDosformCode()+item.getDrugType()) != null){
+                continue;
+            }else{
+                item.setId(IdWorker.getInstance().getId());
+                insertDrug.add(item);
+                jkypMap.put(item.getChemicalName()+item.getDrugDosformCode()+item.getDrugType(),item.getId());
+            }
+        }
+        String drugSql = sqlPath + "drugSuccess.txt";
+        writeSpecs(insertDrug,drugSql);
+        //错误信息先写入json，在用网页的工具将json转为excel
+        String drugErrorJson = sqlPath + "drugError.json";
+        writeSpecsError(notInsertDrug,drugErrorJson);
+        return "ok";
+    }
+
+    public void writeSpecs(List<UscDrgSpecsDto> drugData,String filePath){
+
+        String sql = "";
+        for (UscDrgSpecsDto item :drugData){
+            sql += String.format("INSERT INTO usc_drg_specs (ID,STANDARD_CODE,CHEMICAL_NAME,DRUG_DOSFORM_CODE," +
+                            "DRUG_TYPE,SPEC_DESC,VERSION_ID,IMPORTED)" +
+                            "VALUES ('%s', '%s', '%s','%s','%s','%s','%s','%s');\n",
+                    item.getId(),item.getStandardCode(),item.getChemicalName(),item.getDrugDosformCode(),item.getDrugType(),
+                    item.getSpecDesc(),item.getVersionId(),item.getImported());
+        }
+
+        WriteFile.writeToFile(filePath,sql);
+    }
+
+    public void writeSpecsError(List<UscDrgSpecsDto> notInsertDrug,String filePath){
+
+        String json = "[";
+        for (UscDrgSpecsDto item :notInsertDrug){
+            json += JSON.toJSONString(item) + ",\n";
+        }
+        WriteFile.writeToFile(filePath,json);
     }
 }
--- transform-boot/src/main/java/cn/trasen/mcpc/transform/util/ReadExcelUtil.java
+++ transform-boot/src/main/java/cn/trasen/mcpc/transform/util/ReadExcelUtil.java
@@ -2,20 +2,22 @@ package cn.trasen.mcpc.transform.util;
 
 import cn.trasen.mcpc.transform.dto.UscDrgSpecsDto;
 import cn.trasen.mcpc.transform.model.UscDrgSpecs;
+import com.google.common.collect.Maps;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
 import org.slf4j.Logger;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -121,7 +123,7 @@ public class ReadExcelUtil {
                     failure.add(uscDrgSpecs);
                     continue;
                 }
-                uscDrgSpecs.setVersionId(110L);
+                uscDrgSpecs.setVersionId(511098310758469632L);
                 success.add(uscDrgSpecs);
             }
             map.put("success",success);
@@ -134,10 +136,220 @@ public class ReadExcelUtil {
         return map;
     }
 
+
+//    public static void writeExcel(String ){
+//
+//    }
+
     public static void main(String[] args) {
         String path = "D:/Trasen/统一标准目录/药物监管局药品数据/jkyp/details/jkyp.xlsx";
         Map<String, List<UscDrgSpecsDto>> map = readXlsxForjkyp(path);
 
         System.out.println(map);
+    }
+
+    /**
+     * 创建Excel文件
+     * @param filepath filepath 文件全路径
+     * @param sheetName 新Sheet页的名字
+     * @param titles 表头
+     * @param values 每行的单元格
+     */
+    public static boolean writeExcel(String filepath, String sheetName, List<String> titles,
+                                     List<Map<String, Object>> values) throws IOException {
+        boolean success = false;
+        OutputStream outputStream = null;
+        if (StringUtils.isBlank(filepath)) {
+            throw new IllegalArgumentException("文件路径不能为空");
+        } else {
+            String suffiex = getSuffiex(filepath);
+            if (StringUtils.isBlank(suffiex)) {
+                throw new IllegalArgumentException("文件后缀不能为空");
+            }
+            Workbook workbook;
+            if ("xls".equals(suffiex.toLowerCase())) {
+                workbook = new HSSFWorkbook();
+            } else {
+                workbook = new XSSFWorkbook();
+            }
+            // 生成一个表格
+            Sheet sheet;
+            if (StringUtils.isBlank(sheetName)) {
+                // name 为空则使用默认值
+                sheet = workbook.createSheet();
+            } else {
+                sheet = workbook.createSheet(sheetName);
+            }
+            // 设置表格默认列宽度为15个字节
+            sheet.setDefaultColumnWidth((short) 15);
+            // 生成样式
+            Map<String, CellStyle> styles = createStyles(workbook);
+            // 创建标题行
+            Row row = sheet.createRow(0);
+            // 存储标题在Excel文件中的序号
+            Map<String, Integer> titleOrder = Maps.newHashMap();
+            for (int i = 0; i < titles.size(); i++) {
+                Cell cell = row.createCell(i);
+                cell.setCellStyle(styles.get("header"));
+                String title = titles.get(i);
+                cell.setCellValue(title);
+                titleOrder.put(title, i);
+            }
+            // 写入正文
+            Iterator<Map<String, Object>> iterator = values.iterator();
+            // 行号
+            int index = 1;
+            while (iterator.hasNext()) {
+                row = sheet.createRow(index);
+                Map<String, Object> value = iterator.next();
+                for (Map.Entry<String, Object> map : value.entrySet()) {
+                    // 获取列名
+                    String title = map.getKey();
+                    // 根据列名获取序号
+                    int i = titleOrder.get(title);
+                    // 在指定序号处创建cell
+                    Cell cell = row.createCell(i);
+                    // 设置cell的样式
+                    if (index % 2 == 1) {
+                        cell.setCellStyle(styles.get("cellA"));
+                    } else {
+                        cell.setCellStyle(styles.get("cellB"));
+                    }
+                    // 获取列的值
+                    Object object = map.getValue();
+                    // 判断object的类型
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    if (object instanceof Double) {
+                        cell.setCellValue((Double) object);
+                    } else if (object instanceof Date) {
+                        String time = simpleDateFormat.format((Date) object);
+                        cell.setCellValue(time);
+                    } else if (object instanceof Calendar) {
+                        Calendar calendar = (Calendar) object;
+                        String time = simpleDateFormat.format(calendar.getTime());
+                        cell.setCellValue(time);
+                    } else if (object instanceof Boolean) {
+                        cell.setCellValue((Boolean) object);
+                    } else {
+                        if (object != null) {
+                            cell.setCellValue(object.toString());
+                        }
+                    }
+                }
+                index++;
+            }
+
+            try {
+                outputStream = new FileOutputStream(filepath);
+                workbook.write(outputStream);
+                success = true;
+            } finally {
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+                if (workbook != null) {
+                    workbook.close();
+                }
+            }
+            return success;
+        }
+    }
+
+    /**
+     * 获取后缀
+     * @param filepath filepath 文件全路径
+     */
+    private static String getSuffiex(String filepath) {
+        if (StringUtils.isBlank(filepath)) {
+            return "";
+        }
+        int index = filepath.lastIndexOf(".");
+        if (index == -1) {
+            return "";
+        }
+        return filepath.substring(index + 1, filepath.length());
+    }
+
+    /**
+     * 设置格式
+     */
+    private static Map<String, CellStyle> createStyles(Workbook wb) {
+        Map<String, CellStyle> styles = Maps.newHashMap();
+
+        // 标题样式
+        XSSFCellStyle titleStyle = (XSSFCellStyle) wb.createCellStyle();
+        titleStyle.setAlignment(HorizontalAlignment.CENTER); // 水平对齐
+        titleStyle.setVerticalAlignment(VerticalAlignment.CENTER); // 垂直对齐
+        titleStyle.setLocked(true); // 样式锁定
+        titleStyle.setFillForegroundColor(IndexedColors.LIGHT_YELLOW.getIndex());
+        Font titleFont = wb.createFont();
+        titleFont.setFontHeightInPoints((short) 16);
+        titleFont.setBold(true);
+        titleFont.setFontName("微软雅黑");
+        titleStyle.setFont(titleFont);
+        styles.put("title", titleStyle);
+
+        // 文件头样式
+        XSSFCellStyle headerStyle = (XSSFCellStyle) wb.createCellStyle();
+        headerStyle.setAlignment(HorizontalAlignment.CENTER);
+        headerStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex()); // 前景色
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND); // 颜色填充方式
+        headerStyle.setWrapText(true);
+        headerStyle.setBorderRight(BorderStyle.THIN); // 设置边界
+        headerStyle.setRightBorderColor(IndexedColors.BLACK.getIndex());
+        headerStyle.setBorderLeft(BorderStyle.THIN);
+        headerStyle.setLeftBorderColor(IndexedColors.BLACK.getIndex());
+        headerStyle.setBorderTop(BorderStyle.THIN);
+        headerStyle.setTopBorderColor(IndexedColors.BLACK.getIndex());
+        headerStyle.setBorderBottom(BorderStyle.THIN);
+        headerStyle.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+        Font headerFont = wb.createFont();
+        headerFont.setFontHeightInPoints((short) 12);
+        headerFont.setColor(IndexedColors.WHITE.getIndex());
+        titleFont.setFontName("微软雅黑");
+        headerStyle.setFont(headerFont);
+        styles.put("header", headerStyle);
+
+        Font cellStyleFont = wb.createFont();
+        cellStyleFont.setFontHeightInPoints((short) 12);
+        cellStyleFont.setColor(IndexedColors.BLUE_GREY.getIndex());
+        cellStyleFont.setFontName("微软雅黑");
+
+        // 正文样式A
+        XSSFCellStyle cellStyleA = (XSSFCellStyle) wb.createCellStyle();
+        cellStyleA.setAlignment(HorizontalAlignment.CENTER); // 居中设置
+        cellStyleA.setVerticalAlignment(VerticalAlignment.CENTER);
+        cellStyleA.setWrapText(true);
+        cellStyleA.setBorderRight(BorderStyle.THIN);
+        cellStyleA.setRightBorderColor(IndexedColors.BLACK.getIndex());
+        cellStyleA.setBorderLeft(BorderStyle.THIN);
+        cellStyleA.setLeftBorderColor(IndexedColors.BLACK.getIndex());
+        cellStyleA.setBorderTop(BorderStyle.THIN);
+        cellStyleA.setTopBorderColor(IndexedColors.BLACK.getIndex());
+        cellStyleA.setBorderBottom(BorderStyle.THIN);
+        cellStyleA.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+        cellStyleA.setFont(cellStyleFont);
+        styles.put("cellA", cellStyleA);
+
+        // 正文样式B:添加前景色为浅黄色
+        XSSFCellStyle cellStyleB = (XSSFCellStyle) wb.createCellStyle();
+        cellStyleB.setAlignment(HorizontalAlignment.CENTER);
+        cellStyleB.setVerticalAlignment(VerticalAlignment.CENTER);
+        cellStyleB.setFillForegroundColor(IndexedColors.LIGHT_YELLOW.getIndex());
+        cellStyleB.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        cellStyleB.setWrapText(true);
+        cellStyleB.setBorderRight(BorderStyle.THIN);
+        cellStyleB.setRightBorderColor(IndexedColors.BLACK.getIndex());
+        cellStyleB.setBorderLeft(BorderStyle.THIN);
+        cellStyleB.setLeftBorderColor(IndexedColors.BLACK.getIndex());
+        cellStyleB.setBorderTop(BorderStyle.THIN);
+        cellStyleB.setTopBorderColor(IndexedColors.BLACK.getIndex());
+        cellStyleB.setBorderBottom(BorderStyle.THIN);
+        cellStyleB.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+        cellStyleB.setFont(cellStyleFont);
+        styles.put("cellB", cellStyleB);
+
+        return styles;
     }
 }
